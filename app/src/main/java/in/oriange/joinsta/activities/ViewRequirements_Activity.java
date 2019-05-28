5a2f80a7d619aefef5ad1c35fad4b64f40ba5bd8
sk_avantecodeworx <shrikant.kulkarni@avantecodeworx.com>
2019-05-28 18:27:05 +0530

    view requirement and favourite

--- app/src/main/java/in/oriange/joinsta/activities/ViewRequirements_Activity.java
+++ app/src/main/java/in/oriange/joinsta/activities/ViewRequirements_Activity.java
@@ -7,7 +7,14 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import in.oriange.joinsta.R;
 import in.oriange.joinsta.models.RequirementsListModel;
 import in.oriange.joinsta.utilities.UserSessionManager;
@@ -18,6 +25,10 @@ public class ViewRequirements_Activity extends AppCompatActivity {
     private UserSessionManager session;
     private ProgressDialog pd;
 
+    private CircleImageView imv_user;
+    private ImageView imv_mobile, imv_email;
+    private TextView tv_reqby_name;
+    private MaterialEditText edt_categoryname, edt_reqmtitle, edt_reqmdesc, edt_city;
 
     private RequirementsListModel reqDetails;
 
@@ -37,10 +48,41 @@ public class ViewRequirements_Activity extends AppCompatActivity {
         context = ViewRequirements_Activity.this;
         session = new UserSessionManager(context);
         pd = new ProgressDialog(context);
+        imv_user = findViewById(R.id.imv_user);
+        imv_mobile = findViewById(R.id.imv_mobile);
+        imv_email = findViewById(R.id.imv_email);
+        tv_reqby_name = findViewById(R.id.tv_reqby_name);
+        edt_categoryname = findViewById(R.id.edt_categoryname);
+        edt_reqmtitle = findViewById(R.id.edt_reqmtitle);
+        edt_reqmdesc = findViewById(R.id.edt_reqmdesc);
+        edt_city = findViewById(R.id.edt_city);
     }
 
     private void setDefault() {
         reqDetails = (RequirementsListModel) getIntent().getSerializableExtra("reqDetails");
+
+
+        if (!reqDetails.getImageURL().trim().isEmpty()) {
+            Picasso.with(context)
+                    .load(reqDetails.getImageURL().trim())
+                    .placeholder(R.drawable.icon_userphoto)
+                    .into(imv_user, new Callback() {
+                        @Override
+                        public void onSuccess() {
+
+                        }
+
+                        @Override
+                        public void onError() {
+
+                        }
+                    });
+        }
+
+        tv_reqby_name.setText(reqDetails.getFname() + " " + reqDetails.getMname() + " " + reqDetails.getLname());
+        edt_reqmtitle.setText(reqDetails.getTitle());
+        edt_reqmdesc.setText(reqDetails.getDescription());
+        edt_city.setText(reqDetails.getCity());
     }
 
     private void getSessionData() {
@@ -48,7 +90,19 @@ public class ViewRequirements_Activity extends AppCompatActivity {
     }
 
     private void setEventHandler() {
+        imv_mobile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+        imv_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     private void setUpToolbar() {
--- app/src/main/java/in/oriange/joinsta/fragments/Favourite_Fragment.java
+++ app/src/main/java/in/oriange/joinsta/fragments/Favourite_Fragment.java
@@ -1,20 +1,68 @@
 package in.oriange.joinsta.fragments;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.ybq.android.spinkit.SpinKitView;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.skydoves.powermenu.MenuAnimation;
+import com.skydoves.powermenu.MenuEffect;
+import com.skydoves.powermenu.OnMenuItemClickListener;
+import com.skydoves.powermenu.PowerMenu;
+import com.skydoves.powermenu.PowerMenuItem;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import in.oriange.joinsta.R;
+import in.oriange.joinsta.adapters.SearchAdapterBusiness;
+import in.oriange.joinsta.adapters.SearchAdapterEmployee;
+import in.oriange.joinsta.adapters.SearchAdapterProfessional;
+import in.oriange.joinsta.models.MainCategoryListModel;
+import in.oriange.joinsta.models.SearchDetailsModel;
+import in.oriange.joinsta.pojos.MainCategoryListPojo;
+import in.oriange.joinsta.utilities.APICall;
+import in.oriange.joinsta.utilities.ApplicationConstants;
+import in.oriange.joinsta.utilities.UserSessionManager;
+import in.oriange.joinsta.utilities.Utilities;
 
 public class Favourite_Fragment extends Fragment {
 
     private Context context;
+    private UserSessionManager session;
+    private RecyclerView rv_searchlist;
+    private EditText edt_search;
+    private AppCompatEditText edt_type;
+    private SpinKitView progressBar;
+    private PowerMenu iconMenu;
+    public static ArrayList<SearchDetailsModel.ResultBean.BusinessesBean> businessList;
+    public static ArrayList<SearchDetailsModel.ResultBean.ProfessionalsBean> professionalList;
+    public static ArrayList<SearchDetailsModel.ResultBean.EmployeesBean> employeeList;
+
+    private String userId, categoryTypeId;
+    private ProgressDialog pd;
+    private ArrayList<MainCategoryListModel> mainCategoryList;
+    private ArrayList<PowerMenuItem> powerMenuItems;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -28,22 +76,299 @@ public class Favourite_Fragment extends Fragment {
     }
 
     private void init(View rootView) {
+        session = new UserSessionManager(context);
+        pd = new ProgressDialog(context);
         Toolbar toolbar = rootView.findViewById(R.id.toolbar);
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        progressBar = rootView.findViewById(R.id.progressBar);
+        edt_type = rootView.findViewById(R.id.edt_type);
+        edt_search = rootView.findViewById(R.id.edt_search);
+        rv_searchlist = rootView.findViewById(R.id.rv_searchlist);
+        rv_searchlist.setLayoutManager(new LinearLayoutManager(context));
+
+        businessList = new ArrayList<>();
+        professionalList = new ArrayList<>();
+        employeeList = new ArrayList<>();
+
+        mainCategoryList = new ArrayList<>();
+        powerMenuItems = new ArrayList<>();
     }
 
-
     private void setDefault() {
+        categoryTypeId = "1";
+        edt_type.setText("Business");
+
+        if (Utilities.isNetworkAvailable(context)) {
+            new GetSearchList().execute(session.getLocation().get(ApplicationConstants.KEY_LOCATION_INFO));
+        }
 
     }
 
     private void getSessionDetails() {
+        try {
+            JSONArray user_info = new JSONArray(session.getUserDetails().get(
+                    ApplicationConstants.KEY_LOGIN_INFO));
+            JSONObject json = user_info.getJSONObject(0);
+            userId = json.getString("userid");
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class GetSearchList extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+            rv_searchlist.setVisibility(View.GONE);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String res = "[]";
+            JsonObject obj = new JsonObject();
+            obj.addProperty("type", "getDetailsByLocation");
+            obj.addProperty("user_id", userId);
+            obj.addProperty("location", params[0]);
+            res = APICall.JSONAPICall(ApplicationConstants.SEARCHAPI, obj.toString());
+            return res.trim();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressBar.setVisibility(View.GONE);
+            rv_searchlist.setVisibility(View.VISIBLE);
+            String type = "", message = "";
+            try {
+                if (!result.equals("")) {
+                    businessList = new ArrayList<>();
+                    professionalList = new ArrayList<>();
+                    employeeList = new ArrayList<>();
+                    SearchDetailsModel pojoDetails = new Gson().fromJson(result, SearchDetailsModel.class);
+                    type = pojoDetails.getType();
+
+                    if (type.equalsIgnoreCase("success")) {
+                        businessList = pojoDetails.getResult().getBusinesses();
+                        professionalList = pojoDetails.getResult().getProfessionals();
+                        employeeList = pojoDetails.getResult().getEmployees();
+
+                        ArrayList<SearchDetailsModel.ResultBean.BusinessesBean> foundbiz = new ArrayList<SearchDetailsModel.ResultBean.BusinessesBean>();
+                        for(SearchDetailsModel.ResultBean.BusinessesBean bizdetails : businessList){
+                            if(bizdetails.getIsFavourite().equals("0")){
+                                foundbiz.add(bizdetails);
+                            }
+                        }
+                        businessList.removeAll(foundbiz);
+
+                        ArrayList<SearchDetailsModel.ResultBean.ProfessionalsBean> foundProf = new ArrayList<SearchDetailsModel.ResultBean.ProfessionalsBean>();
+                        for(SearchDetailsModel.ResultBean.ProfessionalsBean profdetails : professionalList){
+                            if(profdetails.getIsFavourite().equals("0")){
+                                foundProf.add(profdetails);
+                            }
+                        }
+                        professionalList.removeAll(foundProf);
+
+                        ArrayList<SearchDetailsModel.ResultBean.EmployeesBean> foundEmp = new ArrayList<SearchDetailsModel.ResultBean.EmployeesBean>();
+                        for(SearchDetailsModel.ResultBean.EmployeesBean empdetails : employeeList){
+                            if(empdetails.getIsFavourite().equals("0")){
+                                foundEmp.add(empdetails);
+                            }
+                        }
+                        employeeList.removeAll(foundEmp);
+
+                        setDataToRecyclerView(categoryTypeId);
+                    } else {
+                        Utilities.showAlertDialog(context, "Categories not available", false);
+
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Utilities.showAlertDialog(context, "Server Not Responding", false);
+            }
+        }
     }
 
     private void setEventHandlers() {
+        edt_type.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (powerMenuItems.size() == 0)
+                    if (Utilities.isNetworkAvailable(context))
+                        new GetMainCategotyList().execute();
+                    else
+                        Utilities.showMessage(R.string.msgt_nointernetconnection, context, 2);
+                else
+                    showCategoryMenus(powerMenuItems);
+            }
+        });
 
+        edt_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchDetails(categoryTypeId, s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
+
+    private void searchDetails(String categoryTypeId, String query) {
+        switch (categoryTypeId) {
+            case "1":
+                if (!query.equals("")) {
+                    ArrayList<SearchDetailsModel.ResultBean.BusinessesBean> businessSearchedList = new ArrayList<>();
+                    for (SearchDetailsModel.ResultBean.BusinessesBean businessDetails : businessList) {
+                        String businessToBeSearched = businessDetails.getBusiness_name().toLowerCase() +
+                                businessDetails.getCity().toLowerCase();
+                        if (businessToBeSearched.contains(query.toLowerCase())) {
+                            businessSearchedList.add(businessDetails);
+                        }
+                    }
+                    rv_searchlist.setAdapter(new SearchAdapterBusiness(context, businessSearchedList));
+                } else {
+                    rv_searchlist.setAdapter(new SearchAdapterBusiness(context, businessList));
+                }
+                break;
+            case "2":
+                if (!query.equals("")) {
+                    ArrayList<SearchDetailsModel.ResultBean.ProfessionalsBean> professionalSearchedList = new ArrayList<>();
+                    for (SearchDetailsModel.ResultBean.ProfessionalsBean professionalDetails : professionalList) {
+                        String professionalToBeSearched = professionalDetails.getFirm_name().toLowerCase() +
+                                professionalDetails.getCity().toLowerCase();
+                        if (professionalToBeSearched.contains(query.toLowerCase())) {
+                            professionalSearchedList.add(professionalDetails);
+                        }
+                    }
+                    rv_searchlist.setAdapter(new SearchAdapterProfessional(context, professionalSearchedList));
+                } else {
+                    rv_searchlist.setAdapter(new SearchAdapterProfessional(context, professionalList));
+                }
+                break;
+            case "3":
+                if (!query.equals("")) {
+                    ArrayList<SearchDetailsModel.ResultBean.EmployeesBean> employeeSearchedList = new ArrayList<>();
+                    for (SearchDetailsModel.ResultBean.EmployeesBean employeeDetails : employeeList) {
+                        String employeeToBeSearched = employeeDetails.getOrganization_name().toLowerCase() +
+                                employeeDetails.getCity().toLowerCase();
+                        if (employeeToBeSearched.contains(query.toLowerCase())) {
+                            employeeSearchedList.add(employeeDetails);
+                        }
+                    }
+                    rv_searchlist.setAdapter(new SearchAdapterEmployee(context, employeeSearchedList));
+                } else {
+                    rv_searchlist.setAdapter(new SearchAdapterEmployee(context, employeeList));
+                }
+
+                break;
+        }
+    }
+
+    private class GetMainCategotyList extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd.setMessage("Please wait ...");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String res = "[]";
+            JsonObject obj = new JsonObject();
+            obj.addProperty("type", "getcategorytypes");
+            res = APICall.JSONAPICall(ApplicationConstants.CATEGORYTYPEAPI, obj.toString());
+            return res.trim();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            pd.dismiss();
+            String type = "", message = "";
+            try {
+                if (!result.equals("")) {
+                    mainCategoryList = new ArrayList<>();
+                    MainCategoryListPojo pojoDetails = new Gson().fromJson(result, MainCategoryListPojo.class);
+                    type = pojoDetails.getType();
+                    message = pojoDetails.getMessage();
+
+                    if (type.equalsIgnoreCase("success")) {
+                        mainCategoryList = pojoDetails.getResult();
+                        if (mainCategoryList.size() > 0) {
+
+                            for (int i = 0; i < mainCategoryList.size(); i++) {
+                                powerMenuItems.add(new PowerMenuItem(mainCategoryList.get(i).getType_description()));
+                            }
+                            showCategoryMenus(powerMenuItems);
+
+                        }
+                    } else {
+                        Utilities.showAlertDialog(context, message, false);
+
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Utilities.showAlertDialog(context, "Server Not Responding", false);
+            }
+        }
+    }
+
+    private void showCategoryMenus(ArrayList<PowerMenuItem> powerMenuItems) {
+        iconMenu = new PowerMenu.Builder(context)
+                .addItemList(powerMenuItems)
+                .setOnMenuItemClickListener(onIconMenuItemClickListener)
+                .setAnimation(MenuAnimation.FADE)
+                .setMenuEffect(MenuEffect.BODY)
+                .setMenuRadius(10f)
+                .setMenuShadow(10f)
+                .build();
+        iconMenu.showAsDropDown(edt_type);
+    }
+
+    private void setDataToRecyclerView(String categoryTypeId) {
+        switch (categoryTypeId) {
+            case "1":
+                if (businessList.size() > 0) {
+                    rv_searchlist.setAdapter(new SearchAdapterBusiness(context, businessList));
+                }
+                break;
+            case "2":
+                if (employeeList.size() > 0) {
+                    rv_searchlist.setAdapter(new SearchAdapterEmployee(context, employeeList));
+                }
+                break;
+            case "3":
+                if (professionalList.size() > 0) {
+                    rv_searchlist.setAdapter(new SearchAdapterProfessional(context, professionalList));
+                }
+                break;
+        }
+    }
+
+    private OnMenuItemClickListener<PowerMenuItem> onIconMenuItemClickListener = new OnMenuItemClickListener<PowerMenuItem>() {
+        @Override
+        public void onItemClick(int position, PowerMenuItem item) {
+            edt_type.setText(item.getTitle());
+            categoryTypeId = mainCategoryList.get(position).getId();
+            iconMenu.dismiss();
+            setDataToRecyclerView(categoryTypeId);
+        }
+    };
 }
