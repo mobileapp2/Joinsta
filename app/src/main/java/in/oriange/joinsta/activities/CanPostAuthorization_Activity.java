f237bb97f76159fedbdbcc67636b1459e407c369
sk_avantecodeworx <shrikant.kulkarni@avantecodeworx.com>
2019-12-31 18:29:01 +0530

    bulk can post

---
+++ app/src/main/java/in/oriange/joinsta/activities/CanPostAuthorization_Activity.java
@@ -0,0 +1,16 @@
+package in.oriange.joinsta.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import in.oriange.joinsta.R;
+
+public class CanPostAuthorization_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_can_post_authorization);
+    }
+}
